e0759221f7977002cc8e2e22ebf68d71e5bf24fb
Clement <bosstclement@gmail.com>
2025-09-10 11:57:55 +0200

    finished exercise 3

---
+++ exercise-3.js
@@ -0,0 +1,13 @@
+function sum(array) {
+  // safety check
+  if (!Array.isArray(array) || !array.length) {
+    return 0;
+  }
+
+  if (array.length === 1) {
+    return array[0];
+  }
+  return sum((array = [array[0] + array[1], ...array.slice(2)]));
+}
+
+console.log(sum([1, 2, 3]));
